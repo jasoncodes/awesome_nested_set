62283f9e51675561de16e91b3efa273a450250dc
Jason Weathered <git@jason.backtick.org>
2010-05-29 15:54:09 +1000

    Add level as an optional block parameter for nested_set_option helper

--- lib/awesome_nested_set/helper.rb
+++ lib/awesome_nested_set/helper.rb
@@ -20,14 +20,14 @@ module CollectiveIdea #:nodoc:
         #       "#{'–' * i.level} #{i.name}"
         #     }) %>
         #
-        def nested_set_options(class_or_item, mover = nil)
+        def nested_set_options(class_or_item, mover = nil, &block)
           class_or_item = class_or_item.roots if class_or_item.is_a?(Class)
           items = Array(class_or_item)
           result = []
           items.each do |root|
-            result += root.self_and_descendants.map do |i|
+            result += root.class.map_with_level(root.self_and_descendants) do |i, level|
               if mover.nil? || mover.new_record? || mover.move_possible?(i)
-                [yield(i), i.id]
+                [yield(*[i, level].first(block.arity)), i.id]
               end
             end.compact
           end
--- test/awesome_nested_set/helper_test.rb
+++ test/awesome_nested_set/helper_test.rb
@@ -7,7 +7,7 @@ module CollectiveIdea
         include Helper
         fixtures :categories
         
-        def test_nested_set_options
+        def test_nested_set_options_without_level
           expected = [
             [" Top Level", 1],
             ["- Child 1", 2],
@@ -22,6 +22,21 @@ module CollectiveIdea
           assert_equal expected, actual
         end
         
+        def test_nested_set_options_with_level
+          expected = [
+            [" Top Level", 1],
+            ["- Child 1", 2],
+            ['- Child 2', 3],
+            ['-- Child 2.1', 4],
+            ['- Child 3', 5],
+            [" Top Level 2", 6]
+          ]
+          actual = nested_set_options(Category) do |c, level|
+            "#{'-' * level} #{c.name}"
+          end
+          assert_equal expected, actual
+        end
+        
         def test_nested_set_options_with_mover
           expected = [
             [" Top Level", 1],
@@ -29,8 +44,8 @@ module CollectiveIdea
             ['- Child 3', 5],
             [" Top Level 2", 6]
           ]
-          actual = nested_set_options(Category, categories(:child_2)) do |c|
-            "#{'-' * c.level} #{c.name}"
+          actual = nested_set_options(Category, categories(:child_2)) do |c, level|
+            "#{'-' * level} #{c.name}"
           end
           assert_equal expected, actual
         end
